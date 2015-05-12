55686ddd23d1741d433ff9ff6bed5b3cfddc32bd
nstanevski <nikolay.stanevski@gmail.com>
2015-05-12 13:41:17 +0300

    task#2

---
+++ 02_StringLength/StringLength.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+/*
+ * Write a program that reads from the console a string of maximum 20 characters. 
+ * If the length of the string is less than 20, the rest of the characters 
+ * should be filled with *. Print the resulting string on the console. 
+ */
+
+class StringLength
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        StringBuilder sb;
+        if(input.Length>20){
+            sb = new StringBuilder(input.Substring(0,20));
+        }else{
+            sb = new StringBuilder(input);
+            while(sb.Length<20){
+                sb.Append('*');
+            }
+        }
+        Console.WriteLine(sb.ToString());   
+    }
+}
