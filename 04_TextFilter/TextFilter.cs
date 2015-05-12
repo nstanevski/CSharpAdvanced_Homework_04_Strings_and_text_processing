2c9958e394c0585dc3c9be82e581bcb451dd3150
nstanevski <nikolay.stanevski@gmail.com>
2015-05-12 17:34:21 +0300

    task#4

---
+++ 04_TextFilter/TextFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class TextFilter
+{
+    static void Main()
+    {
+        string[] bannedWords = Console.ReadLine().Split(',').Select(p => p.Trim()).ToArray();
+        string input = Console.ReadLine();
+        StringBuilder textBuilder = new StringBuilder(input);
+
+        foreach (string bannedWord in bannedWords)
+        {
+            string mask = new string('*', bannedWord.Length);
+            textBuilder.Replace(bannedWord, mask);
+        }
+
+        Console.WriteLine(textBuilder.ToString());
+    }
+}
