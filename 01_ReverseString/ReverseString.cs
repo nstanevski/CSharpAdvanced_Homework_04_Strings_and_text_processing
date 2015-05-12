5d489c878340c0e19760e9d262916a873c3a65cf
nstanevski <nikolay.stanevski@gmail.com>
2015-05-12 13:18:37 +0300

    task#1 added

---
+++ 01_ReverseString/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Write a program that reads a string from the console, reverses it 
+// and prints the result back at the console.
+
+class ReverseString
+{
+    static void Main(string[] args)
+    {
+        char[] charArr = Console.ReadLine().ToCharArray();
+        Array.Reverse(charArr);
+        string reversed = new string(charArr);
+        Console.WriteLine(reversed);
+    }
+}
